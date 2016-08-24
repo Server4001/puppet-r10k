ca2ec60c0893aabc81169d1cf25e5be69d6d493e
Brice Bentler <server4001@gmail.com>
2016-08-23 21:23:41 -0700

    Changes templates contents for the blah module

--- modules/blah/manifests/init.pp
+++ modules/blah/manifests/init.pp
@@ -1,7 +1,7 @@
 class blah {
   file { "/home/vagrant/blah.txt":
     ensure => "present",
-    content => inline_template("This is whatever"),
+    content => inline_template("This is blah"),
     owner => "vagrant",
     group => "vagrant",
     mode => 0644,
